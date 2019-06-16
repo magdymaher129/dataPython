7f73d401bf16371cd46c6402ac39773feda09d8f
MAGDY DEMETRI <magdymaher229@gmail.com>
2019-06-16 09:51:27 +0300

    uploaded by me

--- tennisSet.py
+++ tennisSet.py
@@ -1,7 +1,12 @@
 '''
-In tennis, the winner of a set is based on how many games each player wins. The first player to win 6 games is declared the winner unless their opponent had already won 5 games, in which case the set continues until one of the players has won 7 games.
-
-Given two integers score1 and score2, your task is to determine if it is possible for a tennis set to be finished with a final score of score1 : score2.
+In tennis, the winner of a set is based on how many
+games each player wins. The first player to win 6 games
+is declared the winner unless their opponent had 
+already won 5 games, in which case the set continues
+until one of the players has won 7 games.
+Given two integers score1 and score2, your task
+is to determine if it is possible for a tennis set
+to be finished with a final score of score1 : score2.
 '''
 def tennisSet(score1, score2):
     if (score1 ==6 or score2==6)and (score1 <5 or score2 <5 ):
